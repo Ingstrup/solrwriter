3b29db655557ac388aab2d8cf23a32eaf8b47766
Casper Ingstrup Nielsen <>
2018-12-12 12:53:24 +0100

    indentation

--- reader.rb
+++ reader.rb
@@ -3,13 +3,13 @@ $LOAD_PATH.unshift(local_dir)
 
 class Reader
 
-require 'sax-machine'
-include SAXMachine
-require 'open-uri' # for nokogiri open-method
-require 'nokogiri'
-require 'model/record_list_wrapper'
+  require 'sax-machine'
+  include SAXMachine
+  require 'open-uri' # for nokogiri open-method
+  require 'nokogiri'
+  require 'model/record_list_wrapper'
 
-SAXMachine.handler = :nokogiri
+  SAXMachine.handler = :nokogiri
 
   def read
     url1 = 'http://libguides.dtu.dk/oai.php?verb=ListRecords&metadataPrefix=oai_dc&set=az'
